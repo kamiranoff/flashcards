d4053a5270234a4280f6d7152a3db78b38385c40
Anita <a.nita@hotmail.co.uk>
2020-12-17 18:48:58 +0000

    fix tab bar visible on deck detail and playground screens

--- src/navigation/HomeTabs.tsx
+++ src/navigation/HomeTabs.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Screens } from './interface';
-import HomeStack from './HomeStack';
 import Settings from '../screens/Settings';
+import Home from '../screens/Home';
 
 const Tab = createBottomTabNavigator();
 
 const HomeTabs = () => (
   <Tab.Navigator>
-    <Tab.Screen name={Screens.HOME} component={HomeStack} />
+    <Tab.Screen name={Screens.HOME} component={Home} />
     <Tab.Screen name={Screens.SETTINGS} component={Settings} />
   </Tab.Navigator>
 );
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
 import { RootStackParamList, Screens } from './interface';
 import HomeTabs from './HomeTabs';
 import QuestionModal from '../screens/QuestionModal';
 import AnswerModal from '../screens/AnswerModal';
+import DeckDetail from '../screens/DeckDetail';
+import Playground from '../screens/Playground';
 
-const Stack = createStackNavigator<RootStackParamList>();
-// const Stack = createSharedElementStackNavigator<RootStackParamList>();
+const Stack = createSharedElementStackNavigator<RootStackParamList>();
 
 const Navigation = () => (
   <NavigationContainer>
     <Stack.Navigator mode="modal" initialRouteName={Screens.HOME} screenOptions={{ headerShown: false }}>
       <Stack.Screen name={Screens.HOME} component={HomeTabs} />
+      <Stack.Screen
+        name={Screens.DECK_DETAIL}
+        component={DeckDetail}
+        options={() => ({
+          gestureEnabled: false,
+          cardStyleInterpolator: ({ current: { progress } }) => {
+            return {
+              cardStyle: {
+                opacity: progress,
+              },
+            };
+          },
+        })}
+        sharedElements={(route) => {
+          const { id } = route.params;
+          return [{ id: `item.${id}` }, { id: 'general.bg' }];
+        }}
+      />
+      <Stack.Screen name={Screens.PLAYGROUND} component={Playground} />
       <Stack.Screen name={Screens.QUESTION_MODAL} component={QuestionModal} />
       <Stack.Screen name={Screens.ANSWER_MODAL} component={AnswerModal} />
     </Stack.Navigator>
