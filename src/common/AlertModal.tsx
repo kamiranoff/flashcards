280f9aa6a83244f94856af4d9842d01fe9c51aa2
Anita <a.nita@hotmail.co.uk>
2020-12-30 17:12:27 +0000

    improve modal code

--- src/common/AlertModal.tsx
+++ src/common/AlertModal.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import React, { FC } from 'react';
+import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { RootStackParamList, Screens } from '../navigation/interface';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-const AlertModal = ({ navigation }) => (
-  <View
-    style={{
-      flex: 1,
-      backgroundColor: 'transparent',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}
-  >
-    <TouchableOpacity
-      style={{ backgroundColor: 'white', padding: 20 }}
-      onPress={() => navigation.pop()}
-    >
+type AlertScreenNavigationProp = StackNavigationProp<RootStackParamList, Screens.ALERT>;
+
+export interface Props {
+  navigation: AlertScreenNavigationProp;
+}
+
+const AlertModal: FC<Props> = ({ navigation }) => (
+  <View style={styles.container}>
+    <TouchableOpacity style={{ backgroundColor: 'white', padding: 20 }} onPress={() => navigation.pop()}>
       <Text>Modal me</Text>
     </TouchableOpacity>
   </View>
 );
 
-export default AlertModal;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
+export default AlertModal;
