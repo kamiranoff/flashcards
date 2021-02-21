99f67461d97984866911ae29c067492245264f05
Anita <a.nita@hotmail.co.uk>
2021-02-21 15:58:28 +0000

    adds trash icon to the card item component

--- src/screens/DeckDetail/components/CardItem.tsx
+++ src/screens/DeckDetail/components/CardItem.tsx
@@ -5,41 +5,53 @@ import React, { FC } from 'react';
 import { Card } from '../../../redux/reducer';
 import { getPlatformDimension, isSmallDevice } from '../../../utils/device';
 import { theme } from '../../../utils';
+import IconButton from '../../../common/IconButton';
 
 interface Props {
   onPress: (event: GestureResponderEvent) => void;
-  onLongPress: (event: GestureResponderEvent) => void;
+  onTrashPress: (event: GestureResponderEvent) => void;
   card: Pick<Card, 'question' | 'answer'>;
 }
 
-const CardItem: FC<Props> = ({ onPress, onLongPress, card }) => (
-  <TouchableOpacity onLongPress={onLongPress} onPress={onPress}>
-    <View style={styles.content}>
-      <View style={styles.top}>
-        <CustomText size="p" textStyle={styles.label}>
-          Question:
-        </CustomText>
-        <View style={{ marginVertical: 8 }}>
-          <HtmlParser isSliced text={`${card.question}`} />
-        </View>
-      </View>
-      {card.answer.length ? (
-        <View style={styles.emptyView} />
-      ) : (
-        <View style={styles.bottom}>
-          <Icon
-            name="question"
-            bgColor={theme.colors.warning}
-            imgStyle={styles.iconImg}
-            style={styles.icon}
-          />
-          <CustomText size="p" textStyle={{ color: '#646464' }}>
-            missing answer
-          </CustomText>
-        </View>
-      )}
+const CardItem: FC<Props> = ({ onPress, onTrashPress, card }) => (
+  <View style={styles.content}>
+    <View style={styles.trashIconContainer}>
+      <IconButton
+        onPress={onTrashPress}
+        iconName="trash"
+        imgStyle={styles.transparentIconImg}
+        style={styles.trashIcon}
+        hasShadow={false}
+      />
     </View>
-  </TouchableOpacity>
+    <TouchableOpacity onPress={onPress}>
+      <View>
+        <View style={styles.top}>
+          <CustomText size="p" textStyle={styles.label}>
+            Question:
+          </CustomText>
+          <View style={{ marginVertical: 8 }}>
+            <HtmlParser isSliced text={`${card.question}`} />
+          </View>
+        </View>
+        {card.answer.length ? (
+          <View style={styles.emptyView} />
+        ) : (
+          <View style={styles.bottom}>
+            <Icon
+              name="question"
+              bgColor={theme.colors.warning}
+              imgStyle={styles.iconImg}
+              style={styles.icon}
+            />
+            <CustomText size="p" textStyle={{ color: '#646464' }}>
+              Missing answer
+            </CustomText>
+          </View>
+        )}
+      </View>
+    </TouchableOpacity>
+  </View>
 );
 
 const styles = StyleSheet.create({
@@ -49,6 +61,7 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     justifyContent: 'space-between',
     flexDirection: 'column',
+    zIndex: 3,
   },
   top: {
     justifyContent: 'center',
@@ -71,6 +84,24 @@ const styles = StyleSheet.create({
     color: theme.colors.p,
   },
   emptyView: {
+    height: 30,
+  },
+  trashIconContainer: {
+    zIndex: 2,
+    position: 'absolute',
+    flexDirection: 'row',
+    top: -3,
+    right: -8,
+    alignSelf: 'flex-end',
+  },
+  transparentIconImg: {
+    width: 18,
+    height: 18,
+  },
+  trashIcon: {
+    borderWidth: 0.5,
+    borderColor: '#222',
+    width: 30,
     height: 30,
   },
 });
