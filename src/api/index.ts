8f84865574d02e3af706fbe10a6c4770d1ab6554
Anita <a.nita@hotmail.co.uk>
2021-03-20 13:08:52 +0000

    fix typescript

--- src/api/index.ts
+++ src/api/index.ts
@@ -16,7 +16,10 @@ export interface ResponseDeck {
   cards: Card[];
 }
 
-async function savePhoto(file: File, onUploadProgress: any): Promise<string[]> {
+async function savePhoto(
+  file: File,
+  onUploadProgress: (progressEvent: ProgressEvent) => void,
+): Promise<string[]> {
   const formData = new FormData();
   formData.append('photo', file as any);
   const response = await axios.post(`${Config.API_URL}/image`, formData, {
